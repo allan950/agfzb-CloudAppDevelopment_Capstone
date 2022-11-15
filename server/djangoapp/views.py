bac2b35554e7d358a78b210287dc82ffa11cbc38
Allan <allan95800@yahoo.fr>
2022-11-15 15:49:09 -0500

    Upgraded login, logout and signup features

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -22,6 +22,7 @@ logger = logging.getLogger(__name__)
 # ...
 def about(request): 
     context = {}
+    print(User.objects.all())
     return render(request, 'djangoapp/about.html', context)
 
 # Create a `contact` view to return a static contact page
@@ -54,12 +55,33 @@ def login_request(request):
 # ...
 def logout_request(request):
     logout(request)
-    return redirect("django:logout")
+    return redirect("djangoapp:logout")
 
 
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):
 # ...
+def registration_request(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/registration.html', context)
+    elif request.method == 'POST':
+        first_name = request.POST['first_name'] 
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        password = request.POST['password']
+        print(User.objects.get(username=username, password=password))
+        print(authenticate(username, password))
+        if User.objects.get(username=username, password=password) is None:
+            User.objects.create(username=username, password=password, first_name=first_name, last_name=last_name)
+            user = authenticate(username, password)
+            if user is not None:
+                login(request, user)
+                return redirect("djangoapp:index")
+            else:
+                return render(request, 'djangoapp/registration.html', context)
+        else:
+            return render(request, 'djangoapp/registration.html', context)
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
