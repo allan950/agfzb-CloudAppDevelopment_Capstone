362e4b412d0d3c83018b92be5f0c7f7be953761e
allan950 <107777874+allan950@users.noreply.github.com>
2022-11-20 02:21:48 +0100

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -10,8 +10,8 @@ class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
 
-    #def __str__(self):
-    #    return "Name: {0}, Description: {1}".format(name, description)
+    def __str__(self):
+        return "Name: {0}, Description: {1}".format(self.name, self.description)
 
 class CarModel(models.Model):
     make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
@@ -25,8 +25,8 @@ class CarModel(models.Model):
     type = models.CharField(choices=type_choices, max_length=30)
     year = models.DateField()
 
-    #def __str__(self):
-    #    return "Make: " + self.make + ", Dealer Id: " + self.dealer_id + " Name: " + self.name + ", Type: " + self.type + ", Year: " + self.year
+    def __str__(self):
+        return "Name: " + self.name
     
 
 # - Name
@@ -46,6 +46,29 @@ class CarModel(models.Model):
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
 
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
